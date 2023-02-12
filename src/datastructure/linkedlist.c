bba18b337afa92dd145526276dcbe1f934e07d47
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-02-13 12:40:19 +1300

    simplify linkedlist implementation
    Signed-off-by: Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>

--- src/datastructure/linkedlist.c
+++ src/datastructure/linkedlist.c
@@ -5,25 +5,16 @@ void list_head_init(struct list_head *head) {
 	head->prev = head;
 }
 
-static inline void __list_add(struct list_head *new,
-                              struct list_head *prev,
-                              struct list_head *next) {
-        next->prev = new;
-        new->next = next;
-        new->prev = prev;
-        prev->next = new;
-}
-
 void list_add(struct list_head *new, struct list_head *head) {
-	__list_add(new, head, head->next);
+        head->next->prev = new;
+        new->next = head->next;
+        new->prev = head;
+        head->next = new;
 }
 
 void list_del(struct list_head *entry) {
-	struct list_head *prev, *next;
-	prev = entry->prev;
-	next = entry->next;
-	prev->next = next;
-	next->prev = prev;
+	entry->prev->next = entry->next;
+	entry->next->prev = entry->prev;
 	entry->prev = NULL;
 	entry->next = NULL;
 }
