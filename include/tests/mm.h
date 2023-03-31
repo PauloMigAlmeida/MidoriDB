fe081aa3d7899a17ae43b4511b9ea580bab1df4f
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-03-31 20:13:34 +1300

    mm: row: move utility test functions to a diff source file

--- include/tests/mm.h
+++ include/tests/mm.h
@@ -2,6 +2,7 @@
 #define TESTS_MM_H
 
 #include "tests/unittest.h"
+#include "mm/table.h"
 
 /* test suites */
 bool mm_init_suite(void);
@@ -20,5 +21,16 @@ void test_table_insert_row(void);
 void test_table_delete_row(void);
 void test_table_update_row(void);
 
+/* utility functions used across mm test suites */
+size_t count_datablocks(struct table *table);
+struct row* fetch_row(struct table *table, size_t row_num);
+bool check_row_data(struct table *table, size_t row_num, void *expected);
+bool check_row_flags(struct table *table, size_t row_num, struct row_header_flags *expected);
+bool check_row(struct table *table, size_t row_num, struct row_header_flags *exp_header_flags, struct row *row);
+struct datablock* fetch_datablock(struct table *table, size_t idx);
+struct row* build_row(void *data, size_t data_len, int *null_cols_idx, size_t cols_idx_len);
+void create_test_table_fixed_precision_columns(struct table **out, size_t column_count);
+void create_test_table_var_precision_columns(struct table **out, size_t column_precision, size_t column_count);
+void create_test_table_mixed_precision_columns(struct table **out, size_t column_precision, size_t column_count);
 
 #endif /* TESTS_MM_H */
