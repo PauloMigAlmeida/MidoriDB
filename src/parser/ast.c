65bc8e02a70198800a2c040f9e028744975bd9b2
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-05-16 15:20:51 +1200

    parser: ast: still wrecking my brain trying to come up with a good impl

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -12,42 +12,78 @@ static inline bool strstarts(const char *str, const char *prefix)
 	return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
-static bool __must_check create_table_ast(struct stack *out, struct ast_node **root)
+static bool regex_helper(const char *text, const char *exp, struct stack *out)
 {
 	regex_t regex;
-	regmatch_t matches[4];
+	regmatch_t *matches;
+	char *match;
+
+	BUG_ON(regcomp(&regex, exp, REG_EXTENDED));
+
+	matches = calloc(regex.re_nsub + 1, sizeof(*matches));
+	if (!matches)
+		goto err;
+
+	if (regexec(&regex, text, regex.re_nsub + 1, matches, 0))
+		goto err_regex;
+
+	for (size_t i = 1; i < regex.re_nsub + 1; i++) {
+		int start = matches[i].rm_so;
+		int end = matches[i].rm_eo;
+		int length = end - start;
+
+		match = zalloc(length + 1);
+		if (!match)
+			goto err_regex;
+
+		strncpy(match, text + start, length);
+
+		if (!stack_push(out, match, length + 1))
+			goto err_stack;
+
+		printf("Match %lu: %s\n", i, match);
+		free(match);
+	}
+
+	regfree(&regex);
+	free(matches);
+
+	return false;
+
+err_stack:
+	free(match);
+err_regex:
+	stack_free(out);
+	regfree(&regex);
+	free(matches);
+err:
+	return true;
+}
+
+
+static bool __must_check create_table_ast(struct stack *out, struct ast_node **root)
+{
+
+	struct ast_create_node *node;
+	char *content;
+	struct stack st = {0};
 
 	/* sanity checks */
 	BUG_ON(!root || stack_empty(out));
 
-	char *content = (char*)stack_pop(out);
-
-	// Compile the regular expression pattern
-	if (regcomp(&regex, "CREATE\\s(\\d+)", REG_EXTENDED) != 0) {
-		printf("Could not compile regex.\n");
+	stack_init(&st);
+	content = (char*)stack_pop(out);
+	
+	if(!(node = zalloc(sizeof(*node))))
 		return true;
-	}
+	
+	node->node_type = AST_TYPE_CREATE;
 
-	// Execute the regular expression and obtain matches
-	int status = regexec(&regex, content, ARR_SIZE(matches), matches, 0);
-	while (status == 0) {
-		// Extract the captured matches
-		for (int i = 1; i < 4; i++) {
-			int start = matches[i].rm_so;
-			int end = matches[i].rm_eo;
-			int length = end - start;
-			char *match = malloc(length + 1);
-			strncpy(match, content + start, length);
-			match[length] = '\0';
+	regex_helper(content, "CREATE ([0-9]+) ([0-9]+) ([A-Za-z]+)", &st);
 
-			printf("Match %d: %s\n", i, match);
-			free(match);
-		}
-	}
-
-	// Cleanup
+	/* cleanup */
 	free(content);
-	regfree(&regex);
+	stack_free(&st);
 
 	return false;
 }
