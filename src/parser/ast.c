94e92bc1fa5431877897bc68ec36fd5e38fa3aca
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-05-17 19:52:41 +1200

    parser: ast: implement create_column_ast

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -12,22 +12,24 @@ static inline bool strstarts(const char *str, const char *prefix)
 	return strncmp(str, prefix, strlen(prefix)) == 0;
 }
 
-static bool regex_helper(const char *text, const char *exp, struct stack *out)
+static bool __must_check regex_helper(const char *text, const char *exp, struct stack *out)
 {
 	regex_t regex;
 	regmatch_t *matches;
 	char *match;
+	int num_matches;
 
 	BUG_ON(regcomp(&regex, exp, REG_EXTENDED));
+	num_matches = regex.re_nsub + 1;
 
-	matches = calloc(regex.re_nsub + 1, sizeof(*matches));
+	matches = calloc(num_matches, sizeof(*matches));
 	if (!matches)
 		goto err;
 
-	if (regexec(&regex, text, regex.re_nsub + 1, matches, 0))
+	if (regexec(&regex, text, num_matches, matches, 0))
 		goto err_regex;
 
-	for (size_t i = 1; i < regex.re_nsub + 1; i++) {
+	for (int i = 1; i < num_matches; i++) {
 		int start = matches[i].rm_so;
 		int end = matches[i].rm_eo;
 		int length = end - start;
@@ -41,94 +43,103 @@ static bool regex_helper(const char *text, const char *exp, struct stack *out)
 		if (!stack_push(out, match, length + 1))
 			goto err_stack;
 
-		printf("Match %lu: %s\n", i, match);
+		printf("Match %d: %s\n", i, match);
 		free(match);
 	}
 
 	/* post sanity check */
-	BUG_ON((size_t)out->idx != regex.re_nsub - 1);
+	BUG_ON((size_t )out->idx != regex.re_nsub - 1);
 
 	regfree(&regex);
 	free(matches);
 
 	return false;
 
-err_stack:
+	err_stack:
 	free(match);
-err_regex:
+	err_regex:
 	stack_free(out);
 	regfree(&regex);
 	free(matches);
-err:
+	err:
 	return true;
 }
 
-
-static bool __must_check create_table_ast(struct stack *out, struct ast_node **root)
+static struct ast_create_node* __must_check create_table_ast(struct stack *parser, int *i, struct stack *tmp_st)
 {
 
 	struct ast_create_node *node;
 	char *content;
-	struct stack st = {0};
+	struct stack reg_pars = {0};
 
 	/* sanity checks */
-	BUG_ON(!root || stack_empty(out));
+	BUG_ON(stack_empty(parser));
 
-	stack_init(&st);
-	content = (char*)stack_pop(out);
-	
-	if(!(node = zalloc(sizeof(*node))))
-		return true;
-	
+	stack_init(&reg_pars);
+	content = (char*)stack_peek_pos(parser, *i);
+
+	if (!(node = zalloc(sizeof(*node))))
+		goto err;
+
 	node->node_type = AST_TYPE_CREATE;
 
-	regex_helper(content, "CREATE ([0-9]+) ([0-9]+) ([A-Za-z]+)", &st);
+	regex_helper(content, "CREATE ([0-9]+) ([0-9]+) ([A-Za-z][A-Za-z0-9_]*)", &reg_pars);
+
+	node->if_not_exists = atoi((char*)stack_peek_pos(tmp_st, 0)) == 1;
+	node->column_count = atoi((char*)stack_peek_pos(tmp_st, 1));
+	strncpy(node->table_name,
+		(char*)stack_peek_pos(tmp_st, 2),
+		strlen((char*)stack_peek_pos(tmp_st, 2)) + 1);
+
+	//TODO for tomorrow Add columns to node's children
 
 	/* cleanup */
-	free(content);
-	stack_free(&st);
+	stack_free(&reg_pars);
 
-	return false;
+	return node;
+
+	err:
+	stack_free(&reg_pars);
+	return NULL;
+
 }
 
-static void parse_bison_data_type(char* str, struct ast_column_def_node *node)
+static void parse_bison_data_type(char *str, struct ast_column_def_node *node)
 {
 	int val = atoi(str);
 	int type = val / 10000;
-	int precision = val % 10000; 
+	int precision = val % 10000;
 
 	switch (type) {
-		case 4:
+	case 4:
 		case 5:
-			node->type = CT_INTEGER;
-			node->precision = sizeof(uint64_t);
-			break;
-		case 8:
-			node->type = CT_DOUBLE;
-			node->precision = sizeof(double);
-			break;
+		node->type = CT_INTEGER;
+		node->precision = sizeof(uint64_t);
+		break;
+	case 8:
+		node->type = CT_DOUBLE;
+		node->precision = sizeof(double);
+		break;
 		//TODO figure out the right sizeof for DATE and DATETIME
-		case 10:
-			node->type = CT_DATE;
-			node->precision = sizeof(uint64_t);
-			break;
-		case 11:
-			node->type = CT_DATETIME;
-			node->precision = sizeof(uint64_t);
-			break;
-		case 13:
-			node->type = CT_VARCHAR;
-			node->precision = precision;
-			break;
+	case 10:
+		node->type = CT_DATE;
+		node->precision = sizeof(uint64_t);
+		break;
+	case 11:
+		node->type = CT_DATETIME;
+		node->precision = sizeof(uint64_t);
+		break;
+	case 13:
+		node->type = CT_VARCHAR;
+		node->precision = precision;
+		break;
 	}
 }
 
-
-static struct ast_column_def_node* create_column_ast(struct stack *parser, int *i)
+static struct ast_column_def_node* __must_check create_column_ast(struct stack *parser, int *i)
 {
 	struct ast_column_def_node *node;
 	char *str;
-	bool done;
 	struct stack tmp_st = {0};
 
 	/* discard "STARTCOL" as it's irrelevant now */
@@ -142,11 +153,9 @@ static struct ast_column_def_node* create_column_ast(struct stack *parser, int *
 	/* unless specified otherwise, columns are nullable */
 	node->attr_null = true;
 
-	done = false;
+	for (; *i < parser->idx; *i += 1) {
+		str = (char*)stack_peek_pos(parser, *i);
 
-	for( ; *i < parser->idx; *i += 1) {
-		str = (char*)stack_pop(parser);
-		
 		if (strstarts(str, "ATTR PRIKEY")) {
 			node->attr_prim_key = true;
 			node->attr_null = false;
@@ -160,25 +169,22 @@ static struct ast_column_def_node* create_column_ast(struct stack *parser, int *
 			node->attr_null = false;
 			node->attr_not_null = true;
 		} else {
-			done = true;
 			stack_init(&tmp_st);
 			regex_helper(str, "COLUMNDEF ([0-9]+) ([A-Za-z][A-Za-z0-9_]*)", &tmp_st);
-			strncpy(node->name, (char*)stack_peek_pos(&tmp_st, 0), strlen((char*)stack_peek_pos(&tmp_st, 0) + 1)); 
-			parse_bison_data_type((char*)stack_peek_pos(&tmp_st, 1), node);
+			parse_bison_data_type((char*)stack_peek_pos(&tmp_st, 0), node);
+			strncpy(node->name,
+				(char*)stack_peek_pos(&tmp_st, 1),
+				strlen((char*)stack_peek_pos(&tmp_st, 1)) + 1);
 			stack_free(&tmp_st);
-		}
-
-		free(str);
-
-		if (done)
 			break;
+		}
 	}
 
 	return node;
 
-err:
+	err:
 	return NULL;
-}	
+}
 
 struct ast_node* ast_build_tree(struct stack *parser)
 {
@@ -188,30 +194,29 @@ struct ast_node* ast_build_tree(struct stack *parser)
 
 	/* sanity checks */
 	BUG_ON(!parser || stack_empty(parser));
-	BUG_ON(!strstarts((char*)stack_peek(parser), "STMT"));
+	BUG_ON(!strstarts((char* )stack_peek(parser), "STMT"));
 
 	root = NULL;
 	stack_init(&st);
 
-	for (int i=0; i < parser->idx; i++) {
+	for (int i = 0; i < parser->idx; i++) {
 		str = (char*)stack_peek_pos(parser, i);
 
 		if (strstarts(str, "STARTCOL")) {
-			struct ast_column_def_node* node = create_column_ast(parser, &i);
+			struct ast_column_def_node *node = create_column_ast(parser, &i);
 			if (!node)
 				goto err;
-			
-			if (!stack_push(&st, &node, sizeof(*node)))
+
+			if (!stack_unsafe_push(&st, node))
 				goto err;
 
-		} else if(strstarts(str, "CREATE")){
-		
+		} else if (strstarts(str, "CREATE")) {
+
 		} else {
 			fprintf(stderr, "ast_build_tree: %s handler not implement yet\n", (char*)stack_peek(parser));
 			exit(1);
 		}
 	}
-	
 
 	stack_free(&st);
 
--- tests/parser/ast.c
+++ tests/parser/ast.c
@@ -9,10 +9,21 @@
 #include <parser/syntax.h>
 #include <parser/ast.h>
 
+static void print_stack_content(struct stack *st)
+{
+	for (int i = st->idx; i >= 0; i--) {
+		char *ptr = *((char**)(st->arr->data + i * sizeof(uintptr_t)));
+		printf("%s\n", ptr);
+	}
+	printf("\n");
+}
+
 static void build_stack(char *stmt, struct stack *out)
 {
 	CU_ASSERT(stack_init(out));
 	CU_ASSERT_EQUAL(syntax_parse(stmt, out), 0);
+	printf("\n\n");
+	print_stack_content(out);
 }
 
 void test_ast_build_tree(void)
@@ -23,10 +34,10 @@ void test_ast_build_tree(void)
 	build_stack("CREATE TABLE IF NOT EXISTS A ("
 			"f1 INTEGER PRIMARY KEY AUTO_INCREMENT,"
 			"f2 INT UNIQUE,"
-			"f3 DOUBLE NOT NULL,"
-			"f4 FLOAT NULL);", &st);
+			"f3 DOUBLE NOT NULL);",
+			&st);
 
-	//root = ast_build_tree(&st);
+	root = ast_build_tree(&st);
 	//CU_ASSERT_PTR_NOT_NULL(root);
 
 	stack_free(&st);
