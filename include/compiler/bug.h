4936450659019bbe8cf2fd1394f11e7b1ae7f75f
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-06-09 19:21:17 +1200

    compiler: bug: add BUG_ON_CUSTOM_MSG macro for fatal errors

--- include/compiler/bug.h
+++ include/compiler/bug.h
@@ -7,29 +7,35 @@
 #include <errno.h>
 #include "compiler/macro.h"
 
-static inline void die(const char *fmt, ...) 
+static inline void die(const char *fmt, ...)
 {
-        va_list ap;
-        int ret = errno;
+	va_list ap;
+	int ret = errno;
 
-        if (errno)
-                perror("midoridb");
-        else
-                ret = -1;
+	if (errno)
+		perror("midoridb");
+	else
+		ret = -1;
 
-        va_start(ap, fmt);
-        fprintf(stderr, "  ");
-        vfprintf(stderr, fmt, ap); 
-        va_end(ap);
+	va_start(ap, fmt);
+	fprintf(stderr, "  ");
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
 
-        fprintf(stderr, "\n");
-        exit(ret);
+	fprintf(stderr, "\n");
+	exit(ret);
 }
 
 #define BUG_ON(cond)							\
 	do {								\
 		if (unlikely(cond))					\
 			die("BUG_ON: %s:%u", __func__, __LINE__);	\
+	} while (0)
+
+#define BUG_ON_CUSTOM_MSG(cond, msg)						\
+	do {									\
+		if (unlikely(cond))						\
+			die("BUG_ON: %s:%u :: %s", __func__, __LINE__, msg);	\
 	} while (0)
 
 #endif /* COMPILER_BUG_H */
