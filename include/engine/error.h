c5abfcac17f3e46d364fd31a7f333b9af37e34c0
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-06-10 21:17:41 +1200

    engine: move error code to engine subsystem

---
+++ include/engine/error.h
@@ -0,0 +1,17 @@
+/*
+ * error.h
+ *
+ *  Created on: 21/02/2023
+ *      Author: paulo
+ */
+
+#ifndef INCLUDE_COMPILER_ERROR_H_
+#define INCLUDE_COMPILER_ERROR_H_
+
+#define	MIDORIDB_OK			0	/* Successful result */
+#define	MIDORIDB_ERROR			1	/* Generic error */
+#define	MIDORIDB_INTERNAL		2	/* Internal error - apps shouldn't see this */
+#define	MIDORIDB_NOMEM			3	/* Resource couldn't be allocated */
+
+
+#endif /* INCLUDE_COMPILER_ERROR_H_ */
