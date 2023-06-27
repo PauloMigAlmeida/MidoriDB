4c1eb9b4ca6506f427c1978eb05094b7fc0309fb
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-06-27 16:46:25 +1200

    engine: executor: draft implementation for ast_ins_exprval_node

--- src/engine/executor_insert.c
+++ src/engine/executor_insert.c
@@ -7,14 +7,113 @@
 
 #include <engine/executor.h>
 #include <primitive/table.h>
+#include <primitive/column.h>
+#include <primitive/row.h>
 #include <datastructure/linkedlist.h>
 
+static int validate_value_for_column(struct column *column,
+				     struct ast_ins_exprval_node *vals_node,
+				     struct query_output *output)
+{
+	int rc = MIDORIDB_OK;
+	
+	if (vals_node->is_str &&
+	     !(column->type == CT_VARCHAR ||
+	       column->type == CT_DATE ||
+	       column->type == CT_DATETIME))
+		goto err_type;
 
-int executor_run_insertvals_stmt(struct database *db, struct ast_ins_insvals_node *ins_node, struct query_output *output)
+	if (vals_node->is_intnum && column->type != CT_INTEGER)
+		goto err_type;
+
+	if (vals_node->is_approxnum && column->type != CT_DOUBLE)
+		goto err_type;
+
+	//TODO add type for is_bool
+	BUG_ON(vals_node->is_bool);
+
+	return rc;
+
+//TODO add error_handler for other types too... right now everything is a string
+err_type:
+	rc = -MIDORIDB_ERROR;
+	snprintf(output->error.message, 
+		 sizeof(output->error.message) - 1,
+		 /* max str size would exceed buffer's size, so trim it */
+		 "value: '%.256s...' is not suitable for column %d\n",
+		 vals_node->str_val,
+		 column->type);
+	return rc;
+}
+
+static struct ast_ins_exprval_node* eval_expr(struct ast_node *node)
+{
+	UNUSED(node);
+	return NULL;
+}
+
+static int build_row(struct table *table,
+		     struct ast_ins_values_node *vals_node,
+		     struct row *row_out,
+		     struct query_output *output)
+{
+	struct list_head *pos;
+	struct ast_node *entry;
+	struct ast_ins_exprval_node *exprval_entry;
+	int col_pos = 0;
+	size_t row_pos = 0;
+	struct column *column;
+	int rc = MIDORIDB_OK;
+
+	list_for_each(pos, vals_node->node_children_head)
+	{
+		entry = list_entry(pos, typeof(*entry), head);
+		column = &table->columns[col_pos];
+
+		if (entry->node_type == AST_TYPE_INS_EXPROP) {
+			exprval_entry = eval_expr(entry);
+			//TODO implement error handling
+		}
+
+		exprval_entry = (struct ast_ins_exprval_node*)entry;
+
+		if ((rc = validate_value_for_column(column, exprval_entry, output)))
+			goto err;
+		
+		if (exprval_entry->is_intnum) {
+			row_out->data[row_pos] = exprval_entry->int_val;
+		} else if (exprval_entry->is_approxnum) {
+			row_out->data[row_pos] = exprval_entry->double_val;
+		} else if (exprval_entry->is_bool) {
+			row_out->data[row_pos] = exprval_entry->bool_val;
+		} else {
+			char *tmp_str = zalloc(column->precision + 1 /* NUL-char */);
+			if (!tmp_str) {
+				rc = -MIDORIDB_INTERNAL;
+				goto err;
+			}
+			//TODO copy string - you know, rush hour in auckland =/
+
+		}
+		
+
+		col_pos++;
+		row_pos += table_calc_column_space(column);
+	}
+
+	return rc;
+
+err:
+	return rc;
+}
+
+int executor_run_insertvals_stmt(struct database *db, struct ast_ins_insvals_node *ins_node,
+				 struct query_output *output)
 {
 	struct list_head *pos;
 	struct ast_node *entry;
 	struct table *table;
+	struct row *row;
 	int rc = MIDORIDB_OK;
 
 	/*
@@ -45,10 +144,19 @@ int executor_run_insertvals_stmt(struct database *db, struct ast_ins_insvals_nod
 
 		if (entry->node_type == AST_TYPE_INS_VALUES) {
 
-		}
+			row = zalloc(table_calc_row_size(table));
+			if (!row)
+				goto err_row_alloc;
+			
+			if ((rc = build_row(table, (struct ast_ins_values_node*)entry, row, output)))
+				goto err_build_row;
 
-//		table_insert_row(table, row, len);
+			if (!table_insert_row(table, row, table_calc_row_size(table)))
+				goto err_ins_row;
+
+		}
 	}
+
 	output->n_rows_aff = ins_node->row_count;
 
 	/* clean up */
@@ -56,6 +164,10 @@ int executor_run_insertvals_stmt(struct database *db, struct ast_ins_insvals_nod
 
 	return rc;
 
+err_ins_row:
+err_build_row:
+	free(row);
+err_row_alloc:
 err_tbl_lock:
 err_tbl_lookup:
 	database_unlock(db);
