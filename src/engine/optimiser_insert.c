a2e17a2e65f3a5952f39e08b6baed7cd6d934680
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-08-24 08:46:04 +1200

    engine: optimiser: implement expr resolution on insert stmts - part 1

--- src/engine/optimiser_insert.c
+++ src/engine/optimiser_insert.c
@@ -7,12 +7,146 @@
 
 #include <engine/optimiser.h>
 
+//static double calcd(struct ast_ins_exprop_node *op, struct ast_ins_exprval_node *val_1, struct ast_ins_exprval_node *val_2)
+//{
+//	return 0.0;
+//}
+//
+static int calci(struct ast_ins_exprop_node *op, struct ast_ins_exprval_node *val_1, struct ast_ins_exprval_node *val_2)
+{
+	int result = 0;
+
+	if (op->op_type == AST_INS_EXPR_OP_ADD) {
+		result = val_1->int_val + val_2->int_val;
+	} else if (op->op_type == AST_INS_EXPR_OP_SUB) {
+		result = val_1->int_val - val_2->int_val;
+	} else if (op->op_type == AST_INS_EXPR_OP_MUL) {
+		result = val_1->int_val * val_2->int_val;
+	} else if (op->op_type == AST_INS_EXPR_OP_DIV) {
+		//TODO implement DIB by 0 stuff
+		result = val_1->int_val / val_2->int_val;
+	} else if (op->op_type == AST_INS_EXPR_OP_MOD) {
+		result = val_1->int_val * val_2->int_val;
+	} else {
+		BUG_ON_CUSTOM_MSG(true, "Operation not implemented yet\n");
+	}
+
+	return result;
+}
+
+static int resolve_math_expr(struct ast_node **node, struct query_output *output)
+{
+	struct list_head *pos;
+	struct ast_node *tmp_entry_1 = NULL;
+	struct ast_node *tmp_entry_2 = NULL;
+
+	struct ast_ins_exprop_node *op;
+	struct ast_ins_exprval_node *val_1;
+	struct ast_ins_exprval_node *val_2;
+	int ret = MIDORIDB_OK;
+
+	/* sanity check */
+	BUG_ON(list_length((*node)->node_children_head) != 2);
+
+	list_for_each(pos, (*node)->node_children_head)
+	{
+		if (!tmp_entry_1)
+			tmp_entry_1 = list_entry(pos, typeof(*tmp_entry_1), head);
+		else
+			tmp_entry_2 = list_entry(pos, typeof(*tmp_entry_2), head);
+	}
+
+	// recursive cases
+	if (!list_is_empty(tmp_entry_1->node_children_head)) {
+
+		if ((ret = resolve_math_expr(&tmp_entry_1, output)))
+			return ret;
+	}
+
+	if (!list_is_empty(tmp_entry_2->node_children_head)) {
+
+		if ((ret = resolve_math_expr(&tmp_entry_2, output)))
+			return ret;
+	}
+
+	// base case -> when we get to the bottom of the math expr
+	op = (typeof(op))*node;
+	val_1 = (typeof(val_1))tmp_entry_1;
+	val_2 = (typeof(val_2))tmp_entry_2;
+
+	/* sanity check - although semantic phase should ensure that this won't ever happen */
+	BUG_ON(memcmp(&val_1->value_type, &val_2->value_type, sizeof(val_1->value_type)));
+
+	/* calculating the result of that operation */
+	if (val_1->value_type.is_intnum) {
+		val_1->int_val = calci(op, val_1, val_2);
+	} else if (val_1->value_type.is_approxnum) {
+		//			val_1->int_val = calcd(op, val_1, val_2);
+	} else {
+		// note to myself: this will come in handy when dealing with div by 0
+		BUG_ON_CUSTOM_MSG(true, "Operation not implemented yet\n");
+	}
+
+	/* juggling with pointers so I don't have to alloc something new on the heap */
+	list_del(&val_1->head);
+	list_add(&val_1->head, op->head.prev);
+	list_del(&op->head);
+	ast_free(*node);
+
+	*node = tmp_entry_1;
+
+	return ret;
+}
+
+static int optimise_math_expr(struct ast_ins_insvals_node *node, struct query_output *output)
+{
+	struct list_head *pos1;
+	struct list_head *pos2;
+	struct list_head *tmp_pos;
+	struct ast_ins_values_node *vals_entry;
+	struct ast_node *tmp_entry;
+	int ret = MIDORIDB_OK;
+
+	list_for_each(pos1, node->node_children_head)
+	{
+		tmp_entry = list_entry(pos1, typeof(*tmp_entry), head);
+
+		if (tmp_entry->node_type == AST_TYPE_INS_VALUES) {
+
+			vals_entry = (typeof(vals_entry))tmp_entry;
+
+			list_for_each_safe(pos2, tmp_pos, vals_entry->node_children_head)
+			{
+				tmp_entry = list_entry(pos2, typeof(*tmp_entry), head);
+
+				if (tmp_entry->node_type == AST_TYPE_INS_EXPROP) {
+					// we are in business
+
+					if ((ret = resolve_math_expr(&tmp_entry, output)))
+						return ret;
+
+					/* if we couldn't resolve the expression, it means something went really bad */
+					BUG_ON(tmp_entry->node_type != AST_TYPE_INS_EXPRVAL);
+
+				}
+			}
+		}
+
+	}
+
+	/* either everything went smooth or there wasn't any math expression to optimise */
+	return ret;
+}
+
+//TODO implement Math expressions resolving optimisation (if any).
 int optimiser_run_insertvals_stmt(struct database *db, struct ast_ins_insvals_node *node, struct query_output *output)
 {
 	UNUSED(db);
-	UNUSED(node);
-	UNUSED(output);
+	int ret;
 
-	//TODO implement Math expressions resolving optimisation (if any).
+	/* try to solve recurssive math expressions (if any) */
+	if ((ret = optimise_math_expr(node, output)))
+		return ret;
+
 	return MIDORIDB_OK;
 }
--- tests/engine/optimiser.c
+++ tests/engine/optimiser.c
@@ -3,11 +3,80 @@
  *
  *  Created on: 22/08/2023
  *      Author: paulo
+ *
+ * Notes to myself:
+ *
+ * - the optimiser tests skips (on purpose) semantic evaluation. This
+ * 	allows me to focus on the changes made to AST without
+ * 	having to worry about running preparation commands such as CREATE
+ * 	tables and so on.
+ *
  */
 
 #include <tests/engine.h>
+#include <engine/query.h>
+#include <parser/ast.h>
+#include <engine/optimiser.h>
+
+extern struct ast_node* build_ast(char *stmt);
+
+static void test_insert_1(void)
+{
+	struct database db = {0};
+	struct query_output output = {0};
+	struct ast_node *node;
+	struct ast_ins_insvals_node *insert_node;
+	struct list_head *pos1, *pos2;
+	struct ast_ins_values_node *vals_entry;
+	struct ast_ins_exprval_node *expval_entry;
+
+	CU_ASSERT_EQUAL(database_open(&db), MIDORIDB_OK);
+	CU_ASSERT_EQUAL(db.tables->count, 0);
+
+	node = build_ast("INSERT INTO A VALUES ((2+2)*3);");
+	CU_ASSERT_EQUAL(optimiser_run(&db, node, &output), MIDORIDB_OK);
+
+	insert_node = (typeof(insert_node))node;
+	CU_ASSERT_EQUAL(insert_node->node_type, AST_TYPE_INS_INSVALS);
+	CU_ASSERT_EQUAL(insert_node->row_count, 1);
+	CU_ASSERT_EQUAL(list_length(insert_node->node_children_head), 1);
+	CU_ASSERT_STRING_EQUAL(insert_node->table_name, "A");
+	CU_ASSERT(list_is_empty(&insert_node->head));
+	CU_ASSERT_FALSE(insert_node->opt_column_list);
+
+	list_for_each(pos1, insert_node->node_children_head)
+	{
+		vals_entry = list_entry(pos1, typeof(*vals_entry), head);
+		CU_ASSERT_EQUAL(vals_entry->node_type, AST_TYPE_INS_VALUES);
+		CU_ASSERT_FALSE(list_is_empty(&vals_entry->head));
+		CU_ASSERT_EQUAL(list_length(vals_entry->node_children_head), 1);
+
+		list_for_each(pos2, vals_entry->node_children_head)
+		{
+			expval_entry = list_entry(pos2, typeof(*expval_entry), head);
+
+			CU_ASSERT_EQUAL(expval_entry->node_type, AST_TYPE_INS_EXPRVAL);
+			CU_ASSERT_FALSE(list_is_empty(&expval_entry->head));
+			CU_ASSERT_EQUAL(list_length(expval_entry->node_children_head), 0);
+
+			CU_ASSERT(expval_entry->value_type.is_intnum);
+			CU_ASSERT_EQUAL(expval_entry->int_val, 12);
+
+		}
+
+	}
+
+	ast_free(node);
+	database_close(&db);
+}
+
+static void insert_cases(void)
+{
+	/* single expr; int value; single recurssion; no div-by-0 */
+	test_insert_1();
+}
 
 void test_optimiser_run(void)
 {
-
+	insert_cases();
 }
