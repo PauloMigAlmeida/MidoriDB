4823df3bae2e2b1c32da0f43c9e69bb361a23e0d
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-08-24 18:51:43 +1200

    engine: optimiser: add final math recursive expr test
    Gee, I've been working on INSERT stmts for a while now... I'm glad that
    this is over (until a come back to implement INSERT INTO ... SELECT)
    
    One problem at the time, ay? :-)

--- tests/engine/optimiser.c
+++ tests/engine/optimiser.c
@@ -470,6 +470,55 @@ static void test_insert_9(void)
 	database_close(&db);
 }
 
+static void test_insert_10(void)
+{
+	struct database db = {0};
+	struct query_output output = {0};
+	struct ast_node *node;
+	struct ast_ins_insvals_node *insert_node;
+	struct list_head *pos1, *pos2;
+	struct ast_ins_values_node *vals_entry;
+	struct ast_ins_exprval_node *expval_entry;
+
+	CU_ASSERT_EQUAL(database_open(&db), MIDORIDB_OK);
+	CU_ASSERT_EQUAL(db.tables->count, 0);
+
+	node = build_ast("INSERT INTO A VALUES (6.0 / (3.0 * (9.0 -(-(5.0 / 5.0))) / 4.0));");
+	CU_ASSERT_EQUAL(optimiser_run(&db, node, &output), MIDORIDB_OK);
+
+	insert_node = (typeof(insert_node))node;
+	CU_ASSERT_EQUAL(insert_node->node_type, AST_TYPE_INS_INSVALS);
+	CU_ASSERT_EQUAL(insert_node->row_count, 1);
+	CU_ASSERT_EQUAL(list_length(insert_node->node_children_head), 1);
+	CU_ASSERT_STRING_EQUAL(insert_node->table_name, "A");
+	CU_ASSERT(list_is_empty(&insert_node->head));
+	CU_ASSERT_FALSE(insert_node->opt_column_list);
+
+	list_for_each(pos1, insert_node->node_children_head)
+	{
+		vals_entry = list_entry(pos1, typeof(*vals_entry), head);
+		CU_ASSERT_EQUAL(vals_entry->node_type, AST_TYPE_INS_VALUES);
+		CU_ASSERT_FALSE(list_is_empty(&vals_entry->head));
+		CU_ASSERT_EQUAL(list_length(vals_entry->node_children_head), 1);
+
+		list_for_each(pos2, vals_entry->node_children_head)
+		{
+			expval_entry = list_entry(pos2, typeof(*expval_entry), head);
+
+			CU_ASSERT_EQUAL(expval_entry->node_type, AST_TYPE_INS_EXPRVAL);
+			CU_ASSERT_FALSE(list_is_empty(&expval_entry->head));
+			CU_ASSERT_EQUAL(list_length(expval_entry->node_children_head), 0);
+
+			CU_ASSERT(expval_entry->value_type.is_approxnum);
+			CU_ASSERT_EQUAL(expval_entry->double_val, 0.8);
+		}
+
+	}
+
+	ast_free(node);
+	database_close(&db);
+}
+
 static void insert_cases(void)
 {
 	/* single expr; int value; single recurssion; no div-by-0 */
@@ -491,6 +540,8 @@ static void insert_cases(void)
 	test_insert_8();
 	/* multi expr; double value; multi recurssion; div-by-0 */
 	test_insert_9();
+	/* single expr; double value; multi recurssion; negation; no div-by-0 */
+	test_insert_10();
 }
 
 void test_optimiser_run(void)
