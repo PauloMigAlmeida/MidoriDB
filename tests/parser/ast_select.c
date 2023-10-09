43db68663fa2a0d812c7fd7e4f787752dccf697a
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2023-10-09 22:03:24 +1300

    parser: ast: add more tests for SELECT statements

--- tests/parser/ast_select.c
+++ tests/parser/ast_select.c
@@ -1135,6 +1135,187 @@ static void select_case_16(void)
 	ast_free(root);
 }
 
+static void select_case_17(void)
+{
+	struct queue ct = {0};
+	struct ast_node *root;
+	struct ast_sel_select_node *select_node;
+	struct ast_sel_fieldname_node *field_node;
+	struct ast_sel_table_node *table_node;
+	struct ast_sel_orderbylist_node *orderbylist_node;
+	struct ast_sel_orderbyitem_node *orderbyitem_node;
+	struct list_head *pos1, *pos2, *pos3;
+	int i = 0;
+
+	parse_stmt("SELECT A.name FROM A ORDER BY A.name DESC;", &ct);
+
+	root = ast_build_tree(&ct);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(root);
+
+	select_node = (typeof(select_node))root;
+	CU_ASSERT_EQUAL(select_node->node_type, AST_TYPE_SEL_SELECT);
+	CU_ASSERT_FALSE(select_node->distinct);
+	CU_ASSERT(list_is_empty(&select_node->head));
+	CU_ASSERT_EQUAL(list_length(select_node->node_children_head), 3);
+
+	list_for_each(pos1, select_node->node_children_head)
+	{
+		if (i == 0) {
+			field_node = list_entry(pos1, typeof(*field_node), head);
+			CU_ASSERT_EQUAL(field_node->node_type, AST_TYPE_SEL_FIELDNAME);
+			CU_ASSERT_STRING_EQUAL(field_node->col_name, "name");
+			CU_ASSERT_STRING_EQUAL(field_node->table_name, "A");
+			CU_ASSERT_FALSE(list_is_empty(&field_node->head));
+			CU_ASSERT_EQUAL(list_length(field_node->node_children_head), 0);
+		} else if (i == 1) {
+			table_node = list_entry(pos1, typeof(*table_node), head);
+			CU_ASSERT_EQUAL(table_node->node_type, AST_TYPE_SEL_TABLE);
+			CU_ASSERT_STRING_EQUAL(table_node->table_name, "A");
+			CU_ASSERT_FALSE(list_is_empty(&table_node->head));
+			CU_ASSERT_EQUAL(list_length(table_node->node_children_head), 0);
+		} else {
+			orderbylist_node = list_entry(pos1, typeof(*orderbylist_node), head);
+			CU_ASSERT_EQUAL(orderbylist_node->node_type, AST_TYPE_SEL_ORDERBYLIST);
+			CU_ASSERT_FALSE(list_is_empty(&orderbylist_node->head));
+			CU_ASSERT_EQUAL(list_length(orderbylist_node->node_children_head), 1);
+
+			list_for_each(pos2, orderbylist_node->node_children_head)
+			{
+				orderbyitem_node = list_entry(pos2, typeof(*orderbyitem_node), head);
+				CU_ASSERT_EQUAL(orderbyitem_node->node_type, AST_TYPE_SEL_ORDERBYITEM);
+				CU_ASSERT_EQUAL(orderbyitem_node->direction, AST_SEL_ORDERBY_DESC);
+				CU_ASSERT_FALSE(list_is_empty(&orderbyitem_node->head));
+				CU_ASSERT_EQUAL(list_length(orderbyitem_node->node_children_head), 1);
+
+				list_for_each(pos3, orderbyitem_node->node_children_head)
+				{
+					field_node = list_entry(pos3, typeof(*field_node), head);
+					CU_ASSERT_EQUAL(field_node->node_type, AST_TYPE_SEL_FIELDNAME);
+					CU_ASSERT_STRING_EQUAL(field_node->col_name, "name");
+					CU_ASSERT_STRING_EQUAL(field_node->table_name, "A");
+					CU_ASSERT_FALSE(list_is_empty(&field_node->head));
+					CU_ASSERT_EQUAL(list_length(field_node->node_children_head), 0);
+				}
+			}
+		}
+		i++;
+
+	}
+
+	queue_free(&ct);
+	ast_free(root);
+}
+
+static void select_case_18(void)
+{
+	struct queue ct = {0};
+	struct ast_node *root;
+	struct ast_sel_select_node *select_node;
+	struct ast_sel_fieldname_node *field_node;
+	struct ast_sel_table_node *table_node;
+	struct ast_sel_groupby_node *groupby_node;
+	struct ast_sel_having_node *having_node;
+	struct ast_sel_cmp_node *cmp_node;
+	struct ast_sel_exprval_node *val_node;
+	struct ast_sel_count_node *count_node;
+	struct list_head *pos1, *pos2, *pos3, *pos4;
+	int i = 0, j = 0;
+
+	parse_stmt("SELECT A.f2 FROM A GROUP BY A.f1 HAVING COUNT(A.f1) > 5;", &ct);
+
+	root = ast_build_tree(&ct);
+	CU_ASSERT_PTR_NOT_NULL_FATAL(root);
+
+	select_node = (typeof(select_node))root;
+	CU_ASSERT_EQUAL(select_node->node_type, AST_TYPE_SEL_SELECT);
+	CU_ASSERT_FALSE(select_node->distinct);
+	CU_ASSERT(list_is_empty(&select_node->head));
+	CU_ASSERT_EQUAL(list_length(select_node->node_children_head), 4);
+
+	list_for_each(pos1, select_node->node_children_head)
+	{
+		if (i == 0) {
+			field_node = list_entry(pos1, typeof(*field_node), head);
+			CU_ASSERT_EQUAL(field_node->node_type, AST_TYPE_SEL_FIELDNAME);
+			CU_ASSERT_STRING_EQUAL(field_node->col_name, "f2");
+			CU_ASSERT_STRING_EQUAL(field_node->table_name, "A");
+			CU_ASSERT_FALSE(list_is_empty(&field_node->head));
+			CU_ASSERT_EQUAL(list_length(field_node->node_children_head), 0);
+		} else if (i == 1) {
+			table_node = list_entry(pos1, typeof(*table_node), head);
+			CU_ASSERT_EQUAL(table_node->node_type, AST_TYPE_SEL_TABLE);
+			CU_ASSERT_STRING_EQUAL(table_node->table_name, "A");
+			CU_ASSERT_FALSE(list_is_empty(&table_node->head));
+			CU_ASSERT_EQUAL(list_length(table_node->node_children_head), 0);
+		} else if (i == 2) {
+			groupby_node = list_entry(pos1, typeof(*groupby_node), head);
+			CU_ASSERT_EQUAL(groupby_node->node_type, AST_TYPE_SEL_GROUPBY);
+			CU_ASSERT_FALSE(list_is_empty(&groupby_node->head));
+			CU_ASSERT_EQUAL(list_length(groupby_node->node_children_head), 1);
+
+			list_for_each(pos2, groupby_node->node_children_head)
+			{
+
+				field_node = list_entry(pos2, typeof(*field_node), head);
+				CU_ASSERT_EQUAL(field_node->node_type, AST_TYPE_SEL_FIELDNAME);
+				CU_ASSERT_STRING_EQUAL(field_node->col_name, "f1");
+				CU_ASSERT_STRING_EQUAL(field_node->table_name, "A");
+				CU_ASSERT_FALSE(list_is_empty(&field_node->head));
+				CU_ASSERT_EQUAL(list_length(field_node->node_children_head), 0);
+			}
+		} else {
+			having_node = list_entry(pos1, typeof(*having_node), head);
+			CU_ASSERT_EQUAL(having_node->node_type, AST_TYPE_SEL_HAVING);
+			CU_ASSERT_FALSE(list_is_empty(&having_node->head));
+			CU_ASSERT_EQUAL(list_length(having_node->node_children_head), 1);
+
+			list_for_each(pos2, having_node->node_children_head)
+			{
+				cmp_node = list_entry(pos2, typeof(*cmp_node), head);
+				CU_ASSERT_EQUAL(cmp_node->node_type, AST_TYPE_SEL_CMP);
+				CU_ASSERT_EQUAL(cmp_node->cmp_type, AST_CMP_GT_OP);
+				CU_ASSERT_FALSE(list_is_empty(&cmp_node->head));
+				CU_ASSERT_EQUAL(list_length(cmp_node->node_children_head), 2);
+
+				list_for_each(pos3, cmp_node->node_children_head)
+				{
+					if (j == 0) {
+						count_node = list_entry(pos3, typeof(*count_node), head);
+						CU_ASSERT_EQUAL(count_node->node_type, AST_TYPE_SEL_COUNT);
+						CU_ASSERT_FALSE(count_node->all);
+						CU_ASSERT_FALSE(list_is_empty(&count_node->head));
+						CU_ASSERT_EQUAL(list_length(count_node->node_children_head), 1)
+
+						list_for_each(pos4, count_node->node_children_head)
+						{
+							field_node = list_entry(pos4, typeof(*field_node), head);
+							CU_ASSERT_EQUAL(field_node->node_type, AST_TYPE_SEL_FIELDNAME);
+							CU_ASSERT_STRING_EQUAL(field_node->col_name, "f1");
+							CU_ASSERT_STRING_EQUAL(field_node->table_name, "A");
+							CU_ASSERT_FALSE(list_is_empty(&field_node->head));
+							CU_ASSERT_EQUAL(list_length(field_node->node_children_head), 0);
+						}
+
+					} else {
+						val_node = list_entry(pos3, typeof(*val_node), head);
+						CU_ASSERT_EQUAL(val_node->node_type, AST_TYPE_SEL_EXPRVAL);
+						CU_ASSERT(val_node->value_type.is_intnum);
+						CU_ASSERT_EQUAL(val_node->int_val, 5);
+						CU_ASSERT_FALSE(list_is_empty(&val_node->head));
+						CU_ASSERT_EQUAL(list_length(val_node->node_children_head), 0);
+					}
+					j++;
+				}
+			}
+		}
+		i++;
+
+	}
+
+	queue_free(&ct);
+	ast_free(root);
+}
+
 void test_ast_build_tree_select(void)
 {
 	/* SELECTNODATA */
@@ -1165,8 +1346,12 @@ void test_ast_build_tree_select(void)
 	select_case_13();
 	/* multiple fields + multiple tables + multiple joins + no where-clause + qualified field names */
 	select_case_14();
-	/* multiple fields, single table, no where-clause, group by */
+	/* multiple fields + single table + no where-clause + group by */
 	select_case_15();
-	/* single field, single table, no where-clause, order by, default */
+	/* single field + single table + no where-clause + order by - default */
 	select_case_16();
+	/* single field + single table + no where-clause + order by - explicit direction */
+	select_case_17();
+	/* single field + single table + no where-clause + group by + having  */
+	select_case_18();
 }
