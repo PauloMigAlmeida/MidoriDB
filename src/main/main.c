2085c5bf29db6f1e13b06a023cb004b75799f16e
Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>
2024-03-05 13:45:03 +1300

    Update main.c
    Signed-off-by: Paulo Miguel Almeida <paulo.miguel.almeida.rodenas@gmail.com>

--- src/main/main.c
+++ src/main/main.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "datastructure/linkedlist.h"
-
-struct page_test {
-	int data;
-	struct list_head head;
-};
-
 
 void libmidori_main(void) {
-//	struct page_test *x = malloc(sizeof(*x));
-//	list_head_init(&x->head);
-
-	printf("hello world\n");
-//	free(x);
 	exit(0);
 }
